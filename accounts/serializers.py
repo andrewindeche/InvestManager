77844f1e6434e4ddec59d27f1b2fe24cec3f6bd4
andrewindeche <indecheandrew@gmail.com>
2024-09-09 16:01:41 +0300

    update registration view for token generation

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.tokens import RefreshToken
 
 class LoginSerializer(serializers.ModelSerializer):
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer, LoginSerializer
 
@@ -9,6 +12,18 @@ from .serializers import RegisterSerializer, LoginSerializer
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
 
 class LoginView(generics.CreateAPIView):
     serializer_class = LoginSerializer
