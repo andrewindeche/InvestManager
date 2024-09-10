6c48dade983e448d6282f10bdbe2f3676c57c77a
andrewindeche <indecheandrew@gmail.com>
2024-09-10 20:05:28 +0300

    add pylint to dependencies

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,9 +3,9 @@ from transactions.views import UserTransactionsAdminView
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
-router = DefaultRouter(
-    router.register(r'accounts/(?P<account_pk>\d+)/transactions', TransactionViewSet, basename='transactions')
-)
+router = DefaultRouter()
+router.register(r'accounts/(?P<account_pk>\d+)/transactions', TransactionViewSet, basename='transactions')
+
 
 urlpatterns = [
     path('', include(router.urls)),
