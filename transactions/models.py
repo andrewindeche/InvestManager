020ab04c8857d4a3b3676e17012719d857b57692
andrewindeche <indecheandrew@gmail.com>
2024-09-10 11:26:12 +0300

    add model for transactions

---
+++ transactions/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User, related_name='transactions', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount}"
