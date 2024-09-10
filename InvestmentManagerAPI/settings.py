0490c1b9328917b7c0e8088f2163b5ff0de81566
andrewindeche <indecheandrew@gmail.com>
2024-09-10 09:10:55 +0300

    remove individual env variables

--- InvestmentManagerAPI/settings.py
+++ InvestmentManagerAPI/settings.py
@@ -87,13 +87,9 @@ WSGI_APPLICATION = 'InvestmentManagerAPI.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-        'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'), 
+    'default':{
+        'DATABASE_URL': dj_database_url.config(default=os.getenv('DATABASE_URL')),
+    }
 }
 
 
