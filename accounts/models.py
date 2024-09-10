577a6ca55871f36385b1b09bfc85f383cd721ef3
andrewindeche <indecheandrew@gmail.com>
2024-09-10 11:25:08 +0300

    add model for investor accounts

--- accounts/models.py
+++ accounts/models.py
@@ -10,10 +10,10 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
-class Transaction(models.Model):
-    user = models.ForeignKey(User, related_name='transactions', on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateTimeField()
+class InvestorAccount(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    users = models.ManyToManyField(User, related_name='investor_accounts')
 
     def __str__(self):
-        return f"{self.user.username} - {self.amount}"
+        return self.name
