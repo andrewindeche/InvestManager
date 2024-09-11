b8dd7802f5bade4f1d80c84aa43d3bea24d9b121
andrewindeche <indecheandrew@gmail.com>
2024-09-11 16:57:03 +0300

    serialize transaction models

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,11 +1,50 @@
 from rest_framework import serializers
-from transactions.models import Transaction
+from transactions.models import Transaction, Investment, Holding, InterestReturn
 from accounts.serializers import UserSerializer
 
 class TransactionSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Transaction model.
+    """
     user = UserSerializer(read_only=True)
 
     class Meta:
+        """
+        Metaclass for the Transaction contraints.
+        """
         model = Transaction
         fields = ['id', 'user', 'amount', 'date']
+        
+class InvestmentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Investment model.
+    """
+    class Meta:
+        """
+        Metaclass for the Investment contraints.
+        """
+        model = Investment
+        fields = '__all__'
+        
+class HoldingSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Holding model.
+    """
+    class Meta:
+        """
+        Metaclass for the Holding contraints.
+        """
+        model = Holding
+        fields = '__all__'
+        
+class InterestReturnSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the InterestReturn model.
+    """
+    class Meta:
+        """
+        Metaclass for the InterestReturn contraints.
+        """
+        model = InterestReturn
+        fields = '__all__'
         
