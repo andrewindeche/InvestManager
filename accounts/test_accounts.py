ddbb9b01f0646e2b53263f1f7151951dffc1f3b0
andrewindeche <indecheandrew@gmail.com>
2024-09-13 12:34:34 +0300

    update tests

--- accounts/test_accounts.py
+++ accounts/test_accounts.py
@@ -4,34 +4,29 @@ from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from .models import AccountPermissions,Account
 from django.urls import reverse
-from rest_framework.test import APIClient
 
 # Create your tests here.
 class LoginTestCase(APITestCase):
     """
-    A TestCase for user Login.
+    Test case for user login functionality.
     """
-    def __init__(self, *args, **kwargs):
-        super(LoginTestCase, self).__init__(*args, **kwargs)
-        self.user = None
 
-    def set_up(self):
+    def setUp(self):
         """
-        Set up Test environment with test user and test password.
+        Set up a test user.
         """
-        self.user = User.objects.create_user(username="testuser", password="testpassword", is_active=True)
+        self.url = reverse('login') 
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
 
     def test_successful_login(self):
         """
-        Test the login of a user using test username and password.
-        A POST request is sent and if succesful a response is asserted with a status code of '200' and tokens 
+        Test the login using a test username and password.
         """
-        url = reverse('login')
         data = {
             'username': 'testuser',
             'password': 'testpassword'
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, 200)
         self.assertIn('access', response.data)
         self.assertIn('refresh', response.data)
@@ -60,49 +55,53 @@ class RegistrationTestCase(APITestCase):
         
 class AccountCreationTest(APITestCase):
     """
-    A TestCase for Transaction Account creation.
+    A TestCase for Transaction Account creation and retrieval.
     """
-    
-    def __init__(self, *args, **kwargs):
-        super(AccountCreationTest, self).__init__(*args, **kwargs)
-        self.user = None
-        
-    def set_up(self):
+
+    def setUp(self):
         """
-        Set up a user and authenticate the client.
+        Set up users and authenticate the client.
         """
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client.login(username='testuser', password='testpassword')
-    
-    def test_create_account(self):
+        self.user1 = User.objects.create_user(username='testuser1', password='testpassword1')
+        self.user2 = User.objects.create_user(username='testuser2', password='testpassword2')
+        self.client.login(username='testuser1', password='testpassword1')
+        self.account = Account.objects.create(name='Test Account', description='A test account')
+        self.account.users.add(self.user1, self.user2)
+        self.url = reverse('account-detail', kwargs={'pk': self.account.pk})
+
+    def test_create_account_with_users(self):
         """
-        Test the account creation functionality.
+        Test the account creation functionality with users added by username.
         """
-        url = reverse('accounts-list')
+        url = reverse('account-detail', kwargs={'pk': self.account.pk})
         data = {
             'name': 'Test Investment Account',
             'description': 'This is a test account',
-            'permission': 'full'  
+            'users': ['testuser1', 'testuser2'],
+            'permission': 'full'
         }
-        
+
         response = self.client.post(url, data, format='json')
-        
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
+
         account = Account.objects.get(name='Test Investment Account')
         self.assertEqual(account.description, 'This is a test account')
-        
-        account_permission = AccountPermissions.objects.get(user=self.user, account=account)
+
+        self.assertIn(self.user1, account.users.all())
+        self.assertIn(self.user2, account.users.all())
+
+        account_permission = AccountPermissions.objects.get(user=self.user1, account=account)
         self.assertEqual(account_permission.permission, 'full')
 
     def test_default_permission(self):
         """
         Test account creation with default permission when 'permission' is not provided.
         """
-        url = reverse('accounts-list')
+        url = reverse('account-detail', kwargs={'pk': self.account.pk})
         data = {
             'name': 'Default Permission Account',
-            'description': 'Account with default permission'
+            'description': 'Account with default permission',
+            'users': ['testuser1']
         }
 
         response = self.client.post(url, data, format='json')
@@ -112,5 +111,21 @@ class AccountCreationTest(APITestCase):
         account = Account.objects.get(name='Default Permission Account')
         self.assertEqual(account.description, 'Account with default permission')
 
-        account_permission = AccountPermissions.objects.get(user=self.user, account=account)
+        account_permission = AccountPermissions.objects.get(user=self.user1, account=account)
         self.assertEqual(account_permission.permission, AccountPermissions.VIEW_ONLY)
+
+    def test_retrieve_account(self):
+        """
+        Test retrieving an account.
+        """
+        account = Account.objects.create(name='Retrieve Test Account', description='Test retrieval')
+        account.users.add(self.user1, self.user2)
+
+        url = reverse('account-detail', kwargs={'pk': self.account.pk})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Retrieve Test Account')
+        self.assertEqual(response.data['description'], 'Test retrieval')
+        self.assertIn('testuser1', [user['username'] for user in response.data['users']])
+        self.assertIn('testuser2', [user['username'] for user in response.data['users']])
