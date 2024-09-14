f26f366de9c7e26b9203157a125dacb4709e14ca
andrewindeche <indecheandrew@gmail.com>
2024-09-14 17:28:57 +0300

    update admin models with totals

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,27 +1,32 @@
 from django.contrib import admin
-from .models import Transaction,SimulatedInvestment
-from django.contrib.auth.admin import UserAdmin
+from .models import SimulatedInvestment
+from django.utils.html import format_html
 
-# Register your models here.
-@admin.register(Transaction)
-class TransactionAdmin(admin.ModelAdmin):
-    """
-    Admin interface for managing transactions.
-
-    This class defines how the Transaction model should be presented in the Django admin interface.
-    It allows administrators to view, edit, and manage transaction records, including associating 
-    transactions with users and investments.
-    """
-    list_display = ('user','amount', 'transaction_date', 'transaction_type')
-    search_fields = ('user__username', 'transaction_type')  
-    list_filter = ('transaction_type', 'transaction_date') 
-    
-@admin.register(SimulatedInvestment)
 class SimulatedInvestmentAdmin(admin.ModelAdmin):
     """
-    Admin interface for managing simulated investments.
+    Admin interface for managing simulated investments in kes.
     """
-    readonly_fields = ['price_per_unit']
-    list_display = ('account', 'name', 'symbol', 'units', 'transaction_type', 'transaction_date')
-    search_fields = ('name', 'symbol')
-    list_filter = ('transaction_type', 'transaction_date')
+    list_display = ('account', 'name', 'symbol', 'price_per_unit', 'units', 'total_value_kes', 'user_total_investments_kes')
+    
+    def total_value_kes(self, obj):
+        """
+        Calculate the total value of the current investment in Kenyan Shillings (KES).
+        """
+        total_value = obj.price_per_unit * obj.units
+        kes_value = total_value * 140  # Example conversion rate to KES
+        return format_html(f"KES {kes_value:,.2f}")
+
+    total_value_kes.short_description = 'Total Value (KES)'
+
+    def user_total_investments_kes(self, obj):
+        """
+        Calculate the total value of all investments by the user, in KES.
+        """
+        user_investments = SimulatedInvestment.objects.filter(account=obj.account)
+        total_value = sum([investment.price_per_unit * investment.units for investment in user_investments])
+        kes_total_value = total_value * 140  # Conversion rate to KES
+        return format_html(f"KES {kes_total_value:,.2f}")
+    
+    user_total_investments_kes.short_description = "User's Total Investments (KES)"
+
+admin.site.register(SimulatedInvestment, SimulatedInvestmentAdmin)
