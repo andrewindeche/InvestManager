ce54bb6cd9ae3ed571d25a5d79ef55e9d07f0e2a
andrewindeche <indecheandrew@gmail.com>
2024-09-15 17:38:07 +0300

    add filter for transactions in admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 from .models import SimulatedInvestment
 from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
 class SimulatedInvestmentAdmin(admin.ModelAdmin):
     """
     Admin interface for managing simulated investments in kes.
     """
     list_display = ('account', 'name', 'symbol', 'price_per_unit', 'units', 'total_value_kes', 'user_total_investments_kes')
+    list_filter = (('transaction_date', admin.DateFieldListFilter),)
     
     def total_value_kes(self, obj):
         """
         Calculate the total value of the current investment in Kenyan Shillings (KES).
         """
         total_value = obj.price_per_unit * obj.units
-        kes_value = total_value * 140  # Example conversion rate to KES
+        kes_value = total_value * 140 
         return format_html(f"KES {kes_value:,.2f}")
 
     total_value_kes.short_description = 'Total Value (KES)'
@@ -24,7 +26,7 @@ class SimulatedInvestmentAdmin(admin.ModelAdmin):
         """
         user_investments = SimulatedInvestment.objects.filter(account=obj.account)
         total_value = sum([investment.price_per_unit * investment.units for investment in user_investments])
-        kes_total_value = total_value * 140  # Conversion rate to KES
+        kes_total_value = total_value * 140 
         return format_html(f"KES {kes_total_value:,.2f}")
     
     user_total_investments_kes.short_description = "User's Total Investments (KES)"
