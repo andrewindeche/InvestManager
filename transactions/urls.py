a27c1482eaba2971879cc79872dd7c58d8b7ec39
andrewindeche <indecheandrew@gmail.com>
2024-09-15 19:13:02 +0300

    update urls to use username

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,7 +2,8 @@ from .views import (
      TransactionViewSet,
     InterestReturnViewSet, UserTransactionsAdminView, 
     SimulatedInvestmentTransactionView,
-    PerformanceView,InvestmentViewSet
+    PerformanceView,InvestmentViewSet,
+    InvestmentDateFilterView
     )
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
@@ -17,12 +18,13 @@ router.register(r'investments', InvestmentViewSet, basename='investment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('admin/users/<int:user_id>/transactions/', UserTransactionsAdminView.as_view()),
+    path('admin/users/<str:username>/transactions/', UserTransactionsAdminView.as_view()),
     path(
         'accounts/<int:account_pk>/investments/simulate/', 
         SimulatedInvestmentTransactionView.as_view(), 
         name='simulate-investment-transaction'
     ),
+    path('investments/date-filter/', InvestmentDateFilterView.as_view(), name='investment_date_filter'),
     path('market-data/<int:investment_id>/',  SimulatedInvestmentTransactionView.as_view(), 
          name='market-data'),
     path('performance/', PerformanceView.as_view(), name='performance'),
