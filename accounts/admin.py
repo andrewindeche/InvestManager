bccd4bdd6575d67b107fcd1a27d903cee8f721cc
andrewindeche <indecheandrew@gmail.com>
2024-09-16 17:20:56 +0300

    enforce account permissions for transactions

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,6 +6,15 @@ class AccountAdmin(admin.ModelAdmin):
     """
     Register name and users fields to admin
     """
+    def get_queryset(self, request):
+        """
+        Method to filter staff and non-staff users
+        """
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(users=request.user)
+    
     list_display = ('name',)
     search_fields = ('name',)
     filter_horizontal = ('users',)
