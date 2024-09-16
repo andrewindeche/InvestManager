015ce02ea62fdb31adc3153aa7f0568dedaf938e
andrewindeche <indecheandrew@gmail.com>
2024-09-16 08:31:59 +0300

    update admin view url

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,6 +1,6 @@
 from .views import (
      TransactionViewSet,
-    InterestReturnViewSet, UserTransactionsAdminView, 
+     UserTransactionsAdminView, 
     SimulatedInvestmentTransactionView,
     PerformanceView,InvestmentViewSet,
     InvestmentDateFilterView
@@ -13,19 +13,17 @@ router.register(
 r'accounts/(?P<account_pk>\d+)/transactions', 
  TransactionViewSet,
  basename='transactions')
-router.register(r'interest-returns', InterestReturnViewSet, basename='interest-return')
 router.register(r'investments', InvestmentViewSet, basename='investment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('admin/users/<str:username>/transactions/', UserTransactionsAdminView.as_view()),
+    path('admin/transactions/<str:username>/', UserTransactionsAdminView.as_view()),
     path(
         'accounts/<int:account_pk>/investments/simulate/', 
         SimulatedInvestmentTransactionView.as_view(), 
         name='simulate-investment-transaction'
     ),
     path('investments/date-filter/', InvestmentDateFilterView.as_view(), name='investment_date_filter'),
-    path('market-data/<int:investment_id>/',  SimulatedInvestmentTransactionView.as_view(), 
+    path('market-data/<int:investment_id>/',  PerformanceView.as_view(), 
          name='market-data'),
-    path('performance/', PerformanceView.as_view(), name='performance'),
 ]
