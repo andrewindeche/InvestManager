18532af89591f38889a861d83fb31c7b8c9a1d68
andrewindeche <indecheandrew@gmail.com>
2024-09-18 19:05:52 +0300

    update settings

--- InvestmentManagerAPI/settings.py
+++ InvestmentManagerAPI/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -52,7 +52,6 @@ REST_FRAMEWORK = {
     ),
      'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
     ),
 }
 
