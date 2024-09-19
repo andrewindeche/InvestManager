d77ce38974e6ba46d0d1ee178cabb2553260e512
andrewindeche <indecheandrew@gmail.com>
2024-09-19 13:17:06 +0300

    add transactions to admin to log transactions

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from accounts.models import AccountPermissions
-from .models import SimulatedInvestment
+from .models import SimulatedInvestment, Transaction
 from django.utils.html import format_html
-from django.utils.translation import gettext_lazy as _
 
 class SimulatedInvestmentAdmin(admin.ModelAdmin):
     """
     Admin interface for managing simulated investments in KES.
     """
-    list_display = ('users_list', 'account', 'name', 'symbol', 'price_per_unit', 'units', 'total_value_kes', 'user_total_investments_kes')
+    list_display = ('users_list', 'account', 'name', 'symbol', 'price_per_unit', 'units', 'total_value_kes', 'transaction_date')  # Ensure all fields exist on SimulatedInvestment model
     list_filter = (('transaction_date', admin.DateFieldListFilter),)
-    
+    search_fields = ('account__users__username', 'account__name', 'name')
+
     def get_queryset(self, request):
         """
         Filter and display investments related to accounts accessible to the user.
@@ -26,22 +26,11 @@ class SimulatedInvestmentAdmin(admin.ModelAdmin):
         Calculate the total value of the current investment in Kenyan Shillings (KES).
         """
         total_value = obj.price_per_unit * obj.units
-        kes_value = total_value * 140 
+        kes_value = total_value * 140  
         return format_html(f"KES {kes_value:,.2f}")
 
     total_value_kes.short_description = 'Total Value (KES)'
 
-    def user_total_investments_kes(self, obj):
-        """
-        Calculate the total value of all investments by the user, in KES.
-        """
-        user_investments = SimulatedInvestment.objects.filter(account=obj.account)
-        total_value = sum([investment.price_per_unit * investment.units for investment in user_investments])
-        kes_total_value = total_value * 140  
-        return format_html(f"KES {kes_total_value:,.2f}")
-    
-    user_total_investments_kes.short_description = "User's Total Investments (KES)"
-    
     def users_list(self, obj):
         """
         List all users associated with the account.
@@ -54,15 +43,21 @@ class SimulatedInvestmentAdmin(admin.ModelAdmin):
     
 class TransactionAdmin(admin.ModelAdmin):
     """
-    Admin to manage transactions
+    Admin interface for managing transactions.
     """
+    list_display = ('user', 'account', 'investment', 'amount', 'transaction_date', 'transaction_type')
+    list_filter = ('transaction_date', 'transaction_type')
+    search_fields = ('user__username', 'account__name', 'investment__name')
+
     def get_queryset(self, request):
         """
-        Filter transactions based on user status
+        Filter and display transactions related to users accessible to the admin.
         """
         qs = super().get_queryset(request)
         if request.user.is_superuser:
             return qs
         return qs.filter(user=request.user)
-    
+
+admin.site.register(Transaction, TransactionAdmin)
+
 admin.site.register(SimulatedInvestment, SimulatedInvestmentAdmin)
